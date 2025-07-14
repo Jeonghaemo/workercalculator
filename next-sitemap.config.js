1b67d8a211d909adab3fb7b3040977573f80987c
Jeonghaemo <diek19900307@gmail.com>
2025-07-14 10:37:56 +0900

    사이트맵

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://workercalculator.damoapick.co.kr',
+  generateRobotsTxt: true, // robots.txt도 자동 생성
+  // 필요하면 추가 옵션 설정
+};
