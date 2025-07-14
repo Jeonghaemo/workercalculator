8f3b6b915bae62097e910a617b9ce5c9ae046639
Jeonghaemo <diek19900307@gmail.com>
2025-07-14 11:02:49 +0900

    사이트맵

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: 'https://workercalculator.damoapick.co.kr',
   generateRobotsTxt: true, // robots.txt도 자동 생성
   // 필요하면 추가 옵션 설정
+  additionalRobotsTxt: `#DaumWebMasterTool:8aa7601c99002c9ab59e6778fdf04d72759de4ae89912e332c99389118e6f752:ToC4kkmd+X3LhvT+Y/2iOw==`,
 };
